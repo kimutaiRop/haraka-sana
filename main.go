57acbada0cfc51897595140c3b49306f67a70d19
kimutaiRop <kimrop19@gmail.com>
2024-10-22 19:58:29 +0300

    start database integration

--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
+
 func main() {
 	r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
---
+++ models/init.go
@@ -0,0 +1,42 @@
+package models
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	println("Connecting to database...")
+	godotenv.Load()
+	dsn := "host=" + os.Getenv("POSTGRES_HOST") +
+		" user=" + os.Getenv("POSTGRES_USER") +
+		" password=" + os.Getenv("POSTGRES_PASSWORD") +
+		" dbname=" + os.Getenv("POSTGRES_DB") +
+		" port=" + os.Getenv("POSTGRES_PORT") +
+		" sslmode=disable TimeZone=Africa/Nairobi"
+	loggerMode := logger.Default.LogMode(logger.Info)
+	if os.Getenv("DEBUG") != "1" {
+		loggerMode = logger.Default.LogMode(logger.Silent)
+	}
+
+	var database, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: loggerMode,
+	})
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	DB = database
+
+}
+
+func SeedDatabase() {
+	fmt.Println("Seeding database...")
+}
