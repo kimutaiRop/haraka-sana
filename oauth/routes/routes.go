2e65d484039265654ec0f933b68fbada6ca6fd66
kimutaiRop <kimrop19@gmail.com>
2024-10-24 20:23:38 +0300

    test fix and add session auth

--- oauth/routes/routes.go
+++ oauth/routes/routes.go
@@ -9,13 +9,16 @@ import (
 
 func OauthRoutes(basePath *gin.RouterGroup) {
 	oauth2 := basePath.Group("/oauth2")
-	authorizeGroup := oauth2.Group("/authorize").Use(middleware.SessionMiddleware())
-	authorizeGroup.GET("", handlers.AuthorizeCode)
+
 	oauth2.POST("/token", handlers.AuthorizeToken)
 	oauth2.POST("/client-credentials", handlers.ClientCredentials)
+
+	authorizeGroup := oauth2.Group("/authorize").Use(middleware.SessionMiddleware())
+	authorizeGroup.GET("", handlers.AuthorizeCode)
 
 	organizationRoute := basePath.Group("/organization").
 		Use(middleware.JWTAuthMiddleware())
 	organizationRoute.GET("/apps", handlers.GetMyApps)
 	organizationRoute.POST("/create-app", handlers.CreateNewApp)
+
 }
